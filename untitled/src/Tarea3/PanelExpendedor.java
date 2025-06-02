dc51cf8caaf3c40872b8afeaa49134676e213894
vicho173 <vicente.garcia.araya@gmail.com>
2025-06-01 23:24:26 -0400

    agregue fotos de los productos y botones con sus respectivos numeros

--- untitled/src/Tarea3/PanelExpendedor.java
+++ untitled/src/Tarea3/PanelExpendedor.java
@@ -5,35 +5,62 @@ import java.awt.*;
 
 public class PanelExpendedor extends JPanel {
 
+    private ImageIcon[] productos;
+    private JButton[] botonesSeleccion;
+
     public PanelExpendedor() {
         this.setBackground(Color.lightGray);
         this.setLayout(null); // Diseño absoluto
 
-        // Crear componentes
-        JLabel etiqueta = new JLabel("Panel Expendedor");
-        etiqueta.setBounds(10, 10, 150, 30); // Posición y tamaño de la etiqueta
+        // Cargar imágenes de productos
+        productos = new ImageIcon[5];
+        for (int i = 0; i < 5; i++) {
+            productos[i] = new ImageIcon(getClass().getResource("/imagenes/producto" + (i + 1) + ".png"));
+        }
 
-        // Agregar componentes al panel
-        this.add(etiqueta);
+        // Inicializar botones de selección
+        botonesSeleccion = new JButton[5];
+        for (int i = 0; i < 5; i++) {
+            botonesSeleccion[i] = new JButton(String.valueOf(i + 1));
+            botonesSeleccion[i].setMargin(new Insets(0, 0, 0, 0)); // Quitar espacio extra
+            botonesSeleccion[i].setFocusable(false);
+            this.add(botonesSeleccion[i]);
+        }
     }
-
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); // ¡Siempre llama a esto primero!
+        super.paintComponent(g);
 
-        // Obtener el tamaño del panel
         int panelWidth = this.getWidth();
         int panelHeight = this.getHeight();
 
-        // Calcular las coordenadas para centrar el rectángulo
         int rectWidth = 120, rectHeight = 500;
-        int offsetX = 20; // Desplazamiento hacia la izquierda
+        int offsetX = 20;
         int rectX = (panelWidth - rectWidth) / 2 - offsetX;
         int rectY = (panelHeight - rectHeight) / 2;
 
-        // Dibujar el rectángulo desplazado
+        // Dibujar rectángulo del expendedor
         g.setColor(Color.darkGray);
         g.fillRect(rectX, rectY, rectWidth, rectHeight);
+
+        // Dibujar imágenes y posicionar botones
+        int imageWidth = 100;
+        int imageHeight = 80;
+        int espacio = 10;
+
+        for (int i = 0; i < productos.length; i++) {
+            Image img = productos[i].getImage();
+            int xImg = rectX + (rectWidth - imageWidth) / 2;
+            int yImg = rectY + espacio + i * (imageHeight + espacio);
+
+            // Dibujar imagen
+            g.drawImage(img, xImg, yImg, imageWidth, imageHeight, this);
+
+            // Posicionar botón al lado derecho de la imagen
+            int xBtn = xImg + imageWidth + 10;
+            int yBtn = yImg + (imageHeight - 30) / 2;
+            botonesSeleccion[i].setBounds(xBtn, yBtn, 40, 30);
+        }
     }
 }
